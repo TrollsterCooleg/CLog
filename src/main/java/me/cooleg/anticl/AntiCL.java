8fc6e204f7484d36a069aa37ee27414d29e4eff8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-18 19:41:37 -0500

    Make time adjustable.

--- src/main/java/me/cooleg/anticl/AntiCL.java
+++ src/main/java/me/cooleg/anticl/AntiCL.java
@@ -2,7 +2,6 @@ package me.cooleg.anticl;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +10,6 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-import org.bukkit.scheduler.BukkitTask;
 
 
 import java.time.Instant;
@@ -19,6 +17,8 @@ import java.util.HashMap;
 import java.util.UUID;
 
 public final class AntiCL extends JavaPlugin implements Listener {
+
+    final int logoutTimer = 30;
 
     HashMap<UUID, Long> taggedList = new HashMap<>();
     @Override
@@ -35,10 +35,10 @@ public final class AntiCL extends JavaPlugin implements Listener {
         UUID victim = (e.getEntity()).getUniqueId();
 
         if (taggedList.containsKey(victim)) {
-            taggedList.put(victim, Instant.now().getEpochSecond() + 10);
+            taggedList.put(victim, Instant.now().getEpochSecond() + logoutTimer);
             return;
         }
-        taggedList.put(victim, Instant.now().getEpochSecond() + 10);
+        taggedList.put(victim, Instant.now().getEpochSecond() + logoutTimer);
         e.getEntity().sendMessage(ChatColor.RED + "You have been combat tagged!");
         e.getEntity().sendMessage(ChatColor.RED + "Do not log out until told it is safe to do so.");
     }
